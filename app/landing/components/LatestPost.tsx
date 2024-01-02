c8314dc45ca64112901df6ab159e84b8ffcf0ec7
Fahri <111134259+fahrikurniawan99@users.noreply.github.com>
2024-01-02 23:22:53 +0700

    improve py

--- app/landing/components/LatestPost.tsx
+++ app/landing/components/LatestPost.tsx
@@ -6,7 +6,7 @@ import BlogPreviewCard from "../../components/BlogPreviewCard";
 
 export default function LatestPost() {
   return (
-    <Container className="py-44 tracking-tight">
+    <Container className="py-20 md:py-44 tracking-tight">
       <BlogListHeader
         title=" Unlock the Secrets of Web Development Excellence! 🚀"
         description="Dive into our latest blog for expert insights, tips, and trends that will elevate your online presence. Don't miss out – click here to explore the world of cutting-edge web development now! 💻✨ #WebDevRevolution #ExploreInnovation"
